3b1d4829683b96395775f33947ddc459217e0f61
Rishab Nahata <rishabnahata07@gmail.com>
2020-04-09 09:05:23 +0530

    Check Even function and test added

---
+++ src/main/java/com/sapient/week1/CheckEven.java
@@ -0,0 +1,16 @@
+package com.sapient.week1;
+
+public class CheckEven 
+{
+    public boolean check(int n)
+    {
+    	if(n%2 == 0)
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+    }
+}
